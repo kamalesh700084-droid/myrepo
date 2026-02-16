b90a17bf6ee366aed27d6ce88c48de02e6e7bb0e
Kamalesh Mandal <kamalesh.700084@gmail.com>
2026-02-16 16:27:20 +0530

    Add environment check and logging to index.js

---
+++ app/index.js
@@ -0,0 +1,7 @@
+console.log("Running Application Logic...");
+
+if (process.env.NODE_ENV === 'ci') {
+    console.log("SUCCESS: NODE_ENV is set to 'ci'");
+} else {
+    console.log("WARNING: NODE_ENV is NOT 'ci'");
+}
